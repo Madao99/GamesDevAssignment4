37c81f31d60a76bb00c6d2e3f00255f1bfcf69fc
Madao99 <scottiejulian@hotmail.com>
2020-10-11 17:17:33 +1100

    Scene Updates

---
+++ Assets/Scripts/BorderAnimator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BorderAnimator : MonoBehaviour
+{
+    private Sprite sourceImage;
+    public Sprite border0;
+    public Sprite border1;
+    private bool isRunning = false;
+    void Start()
+    {
+        //sourceImage = gameObject.GetComponent<Image>().sprite;
+        
+    }
+
+    
+    void Update()
+    {
+        if (isRunning == false)
+        {
+            StartCoroutine(AnimateBorder());
+            isRunning = true;
+        }
+
+    }
+
+    IEnumerator AnimateBorder()
+    {
+        
+        yield return new WaitForSecondsRealtime(0.4f);
+        if (gameObject.GetComponent<Image>().sprite == border0)
+        {
+            gameObject.GetComponent<Image>().sprite = border1;
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = border0;
+        }
+        isRunning = false;
+
+    }
+}
